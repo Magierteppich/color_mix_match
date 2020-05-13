c77fef070d3267c1ca76b039e99d70533ad7e6d0
Tian Wang <tian.wang.office@googlemail.com>
2020-05-13 16:22:34 +0200

    adding dominant color and colorfulness calculation

--- 01_notebook/final_project.py
+++ 01_notebook/final_project.py
@@ -70,7 +70,7 @@ import os
 import math
 from os.path import isfile, join
 from os import listdir
-
+ 
 
 def image_brightness (path_to_library):
     
@@ -130,3 +130,69 @@ def calc_hsv (path_to_library):
                              "average_s" : average_s, 
                              "average_v" : average_v}, ignore_index = True)
     return df
+
+
+
+
+def get_dominant_color(path_to_library, k=4):
+    
+    result = []
+    
+    file_list = [f for f in listdir(path_to_library) if isfile(join(path_to_library, f))] 
+
+    for file_path in file_list: 
+        image = cv2.imread(path_to_library + "/" + file_path) # CV2 reads the array in BGR! 
+        
+        if image is None: 
+            print(f"The image {file_path} is not readable.")
+        
+        else: 
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #convert to RGB to get the right order
+            image = image.reshape((image.shape[0] * image.shape[1], 3))
+    
+            #cluster and assign labels to the pixels 
+            clt = KMeans(n_clusters = k)
+            labels = clt.fit_predict(image)
+        
+            #count labels to find most popular
+            label_counts = Counter(labels)
+        
+            #subset out most popular centroid
+            dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
+            image_result = list([file_path]) + list(dominant_color)
+            result.append(image_result)
+            
+    return result #result is a list of sub-lists. Each sub-list contains 4 elements: file_path, r,g,b
+
+
+
+def get_colorfulness (path_to_library):
+    
+    result_colorfulness = []
+    
+    file_list = [f for f in listdir(path_to_library) if isfile(join(path_to_library, f))] 
+    
+    for file_path in file_list: 
+        image = cv2.imread(path_to_library + "/" + file_path) # CV2 reads the array in BGR! 
+        
+        if image is None: 
+            print(f"The image {file_path} is not readable.")
+            
+        else: 
+            (B, G, R) = cv2.split(image.astype("float"))
+
+            rg = np.absolute(R-G)
+            yb = np.absolute(0.5*(R + G) - B)
+            
+            (rbMean, rbStd) = (np.mean(rg), np.std(rg))
+            (ybMean, ybStd) = (np.mean(yb), np.std(yb))
+            
+            stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+            meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+            c_metric = stdRoot + (0.3 * meanRoot) 
+    
+            temp_result = list([file_path]) + list([c_metric])
+            result_colorfulness.append(temp_result)
+    
+    return result_colorfulness #result is a list of sub-lists. Each sub-list contains 4 elements: file_path, r,g,b
+    
